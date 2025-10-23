ffae2155b5973c230398cb2ef22d3da7e62cd9c5
APrajapat10 <coolprajapat@gmail.com>
2025-10-23 10:14:30 +0530

    Chat Application backend changes done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+require("./utils/cronJob");
+const { initializeSocket } = require("./utils/socket");
+const http = require("http");
 const express = require("express");
 const { dbConnect } = require("../src/config/database");
 const { User } = require("./models/user");
@@ -7,6 +10,7 @@ const cookieParser = require("cookie-parser");
 const { authRouter } = require("./routers/auth");
 const { profileRouter } = require("./routers/profile");
 const { requestRouter } = require("./routers/request");
+const { chatRouter } = require("./routers/chat");
 const { userRouter } = require("./routers/user");
 const cors = require("cors");
 app.use(
@@ -22,11 +26,14 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
+const server = http.createServer(app);
+initializeSocket(server);
 dbConnect()
   .then((res) => {
     console.log("Database connected successfully!");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is listening! ");
     });
   })
